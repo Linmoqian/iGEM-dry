357049a9a7709ad881d98a36e0d182cdd0f87a64
JnTngDn <507626133@qq.com>
2026-06-07 02:46:25 +0800

    feat: 取回顶部状态栏，更新图片素材

--- web/src/layouts/AppShell.tsx
+++ web/src/layouts/AppShell.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import SidebarNav from './SidebarNav';
+import AppHeader from './AppHeader';
 
 export default function AppShell() {
   return (
     <div className="flex h-screen overflow-hidden" style={{ background: 'linear-gradient(180deg, #F4F8FC 0%, #E6F0FA 100%)' }}>
       <SidebarNav />
-      <main className="flex-1 overflow-y-auto p-5">
-        <Outlet />
-      </main>
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <AppHeader />
+        <main className="flex-1 overflow-y-auto p-5">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
